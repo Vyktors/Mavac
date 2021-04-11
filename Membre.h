ba2313d6c41e2ab2a96ac5a97a704f15bc24432d
mgrondinleclerc <mgrondinleclerc@gmail.com>
2021-04-11 09:27:04 -0400

    Ajout de la classe Membre

---
+++ Membre.h
@@ -0,0 +1,29 @@
+#ifndef MEMBRE_H
+#define MEMBRE_H
+
+#include <string>
+
+class Membre
+{
+private:
+	std::string m_nomComplet;
+	std::string m_motDePasse;
+	std::string m_email;
+	std::string m_telephone;
+	std::string m_nomFacebook;
+
+public:
+	void setNomComplet(std::string nomComplet);
+	void setMotDePasse(std::string motDePasse);
+	void setEmail(std::string email);
+	void setTelephone(std::string telephone);
+	void setNomFacebook(std::string nomFacebook);
+
+	std::string getNomComplet()const;
+	std::string getMotDePasse()const;
+	std::string getEmail()const;
+	std::string getTelephone()const;
+	std::string getNomFacebook()const;
+};
+
+#endif
